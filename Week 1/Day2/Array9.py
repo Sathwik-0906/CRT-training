6d7b82bf37b24e3ef618af5b3c1d64ab588bbe43
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-07-03 22:35:41 +0530

    added problems

---
+++ Week 1/Day2/Array9.py
@@ -0,0 +1,67 @@
+# 496. Next Greater Element I
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+# You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+# For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+# Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+# Output: [-1,3,-1]
+# Explanation: The next greater element for each value of nums1 is as follows:
+# - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+# - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+# - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+# Example 2:
+
+# Input: nums1 = [2,4], nums2 = [1,2,3,4]
+# Output: [3,-1]
+# Explanation: The next greater element for each value of nums1 is as follows:
+# - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+# - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+
+
+#o(n+m)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack=[]
+        seen={}
+        ans=[]
+        for i in nums2:
+            while stack and i>stack[-1]:
+                prev=stack.pop()
+                seen[prev]=i
+            stack.append(i)
+        for i in range(len(nums1)):
+            if nums1[i] not in seen:
+                ans.append(-1)
+            else:
+                ans.append(seen[nums1[i]])
+        return ans
+    
+
+#o(n*m)=o(n^2)
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans=[-1]*len(nums1)
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i]==nums2[j]:
+                    for k in range(j+1,len(nums2)):
+                        if nums2[k]>nums2[j]:
+                            ans[i]=nums2[k]
+                            break
+        return ans
