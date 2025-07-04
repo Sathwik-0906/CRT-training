4bb5afc82c6366a519b38a9077331c03f6a17a5e
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-07-04 10:08:36 +0530

    added problems

---
+++ Week 1/Day3/Problems/4_.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+    int arr[][3]={{10,20,30},{40,50,60},{70,80,90}};
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(j>i)
+            {
+                
+                printf("0 ");
+                
+            }
+            else
+            {
+                printf("%d ",arr[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+// input:
+// 10 20 30
+// 40 50 60
+// 70 80 90
+
+// output:
+// 10 0 0 
+// 40 50 0 
+// 70 80 90 
