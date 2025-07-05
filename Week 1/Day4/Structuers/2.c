c7f6cb720a178c2caef1eefcef8e9212645879db
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-07-05 23:10:55 +0530

    Day 4

---
+++ Week 1/Day4/Structuers/2.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include<string.h>
+struct bottle{
+    char Name[10];
+    int cost;
+    char Type[20];
+};
+void main() {
+    struct bottle b1;
+    struct bottle *ptr;
+    ptr=&b1;
+    printf("Enter Bottle Name: ");
+    scanf("%s",ptr->Name);
+    printf("Enter Cost: ");
+    scanf("%d",&ptr->cost);
+    printf("Cooling or NO cooling: ");
+    scanf("%s",ptr->Type);
+    
+    printf("%s,%d,%s",ptr->Name,ptr->cost,ptr->Type);
+}
---
+++ Week 1/Day4/Structuers/3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct emp
+{
+char name [10];
+int eid;
+int sal;
+}e1,e2;
+void main()
+me
+{
+strcpy(e1.name, "Krishna");
+el.eid=101;
+e1.sal=45000;
+printf("%s,%d,%d", e1.name, el.eid, el.sal);
+//e2.eid=eid+1; Wrong
+e2.eid=e1.eid+1;
+e2.sale1.sal+10000;
+strcpy(e2.name, "Radha");
+printf("%s,%d,%d", e2.name,e2.eid, e2.sal);
+}
