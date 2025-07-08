091373ef1764b782c03b702029066d7e55e5ca27
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-07-08 11:44:36 +0530

    Double linked list

---
+++ Week 1/Day6/codes on DLL/1intiallization.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* prev;
+    struct node* next;
+}*nn,*head,*temp,*tail;
+
+void insert()
+{
+    int d,val;
+    printf("Entet the No of Node: ");
+    scanf("%d",&d);
+    for(int i=1;i<=d;i++)
+    {
+        nn=(struct node*)malloc(sizeof(struct node));
+        printf("Enter the %d Node data: ",i);
+        scanf("%d",&val);
+        nn->prev=NULL;
+        nn->data=val;
+        nn->next=NULL;
+        if(head==NULL)
+        {
+            head=nn;
+            tail=nn;
+        }
+        else
+        {
+            nn->prev=tail;
+            tail->next=nn;
+            tail=nn;
+        }
+    }
+}
+void display()
+{
+    temp=head;
+    while(temp)
+    {
+        printf("%d",temp->data);
+        temp=temp->next;
+        printf(" --> ");
+    }
+    printf("NULL");
+}
+void main()
+{
+    insert();
+    display();
+}
