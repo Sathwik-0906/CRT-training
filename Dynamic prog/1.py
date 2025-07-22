2d3d40a29544107241e22f064c3c1a2168c5de3a
Sathwik-0906 <gurugubellisathwik@gmail.com>
2025-07-22 10:10:30 +0530

    Dp added

---
+++ Dynamic prog/1.py
@@ -0,0 +1,12 @@
+def feb(n):
+    dp=[0]*(n+1)
+    print(dp)
+    dp[0]=0
+    dp[1]=1 
+    print(dp)
+    for i in range(2,n+1):
+        print(dp[i-1],dp[i-2])
+        dp[i]=dp[i-1]+dp[i-2]
+    return dp[n]
+
+print(feb(5))
